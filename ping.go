21dfc0ad48075c05a70d15eaa8d60e142cd684e0
DoubleCircle-Salt <33078105+DoubleCircle-Salt@users.noreply.github.com>
2022-02-28 15:20:06 +0800

    update

--- ping.go
+++ ping.go
@@ -1,1 +1,98 @@
 package ping
+
+import (
+	"fmt"
+	"net"
+	"time"
+
+	gping "github.com/go-ping/ping"
+)
+
+type Config struct {
+	Count    int
+	Interval time.Duration
+	Timeout  time.Duration
+	Ports    []int
+}
+
+var defaultConfig = &Config{
+	Count:    10,
+	Interval: time.Millisecond,
+	Timeout:  time.Second,
+	Ports:    []int{443, 80},
+}
+
+func icmpPing(ip net.IP, config *Config) (packetLoss, avgRtt float64, err error) {
+	pinger, err := gping.NewPinger(ip.String())
+	if err != nil {
+		return -1, -1, err
+	}
+	pinger.OnFinish = func(stats *gping.Statistics) {
+		packetLoss = stats.PacketLoss
+		avgRtt = float64(stats.AvgRtt.Nanoseconds())/1000000
+	}
+	pinger.Count = config.Count
+	pinger.Interval = config.Interval
+	pinger.Timeout = config.Timeout
+	err = pinger.Run()
+	if err != nil {
+		return -1, -1, err
+	}
+	return
+}
+
+func IcmpPing(ip net.IP, config *Config) (packetLoss, avgRtt float64, err error) {
+	if config == nil {
+		config = defaultConfig
+	}
+	return icmpPing(ip, config)
+}
+
+func tcpPing(ip net.IP, port int, config *Config) (packetLoss, avgRtt float64, err error) {
+	var avgRtt0 float64
+	var count   int
+
+	dialer := &net.Dialer{
+		Timeout: config.Timeout,
+	}
+
+	packetBase := float64(100)/float64(config.Count)
+
+	for i := 0; i < config.Count; i++ {
+		startTime := time.Now()
+		conn, err := dialer.Dial("tcp", fmt.Sprintf("%s:%d", ip.String(), port))
+		if err != nil {
+			packetLoss += packetBase
+			continue
+		}
+		avgRtt0 += float64(time.Now().Sub(startTime).Nanoseconds())/1000000
+		count++
+		conn.Close()
+	}
+	if count != 0 {
+		avgRtt = avgRtt0 / float64(count)
+	}
+	return
+}
+
+func TcpPing(ip net.IP, port int, config *Config) (packetLoss, avgRtt float64, err error) {
+	if config == nil {
+		config = defaultConfig
+	}
+	return tcpPing(ip, port, config)
+}
+
+func ping(ip net.IP, port int, config *Config) (packetLoss, avgRtt float64, err error) {
+	packetLoss, avgRtt, err = icmpPing(ip, config)
+	if err == nil && packetLoss != 100 {
+		return packetLoss, avgRtt, nil
+	}
+	return tcpPing(ip, port, config)
+}
+
+func Ping(ip net.IP, port int, config *Config) (packetLoss, avgRtt float64, err error) {
+	if config == nil {
+		config = defaultConfig
+	}
+	return ping(ip, port, config)
+}
---
+++ ping_test.go
@@ -0,0 +1,43 @@
+package ping
+
+import (
+	"fmt"
+	"net"
+	"testing"
+)
+
+func test_IcmpPing(ip net.IP) {
+
+	packetLoss, avgRtt, err := IcmpPing(ip, nil)
+	if err != nil {
+		fmt.Println("icmpPing failed, err: ", err.Error())
+	}
+
+	fmt.Printf("icmpPing, packetLoss: %.2f%%, avgRtt: %.3fms\n", packetLoss, avgRtt)
+}
+
+func test_TcpPing(ip net.IP, port int) {
+
+	packetLoss, avgRtt, err := TcpPing(ip, port, nil)
+	if err != nil {
+		fmt.Println("tcpPing failed, err: ", err.Error())
+	}
+
+	fmt.Printf("tcpPing, packetLoss: %.2f%%, avgRtt: %.3fms\n", packetLoss, avgRtt)
+}
+
+func test_Ping(ip net.IP, port int) {
+
+	packetLoss, avgRtt, err := Ping(ip, port, nil)
+	if err != nil {
+		fmt.Println("ping failed, err: ", err.Error())
+	}
+
+	fmt.Printf("ping, packetLoss: %.2f%%, avgRtt: %.3fms\n", packetLoss, avgRtt)
+}
+
+func Test_ping(t *testing.T) {
+	test_IcmpPing(net.ParseIP("45.43.41.130"))
+	test_TcpPing(net.ParseIP("45.43.41.130"), 80)
+	test_Ping(net.ParseIP("45.43.41.130"), 80)
+}
